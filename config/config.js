ba260a06bd740a781f5431976cde853dd2c12569
jcostello93 <costejoh@oregonstate.edu>
2019-01-21 20:06:27 -0500

    Create config.js

---
+++ config/config.js
@@ -0,0 +1,18 @@
+module.exports = function(RED) {
+    "use strict";
+
+    const snoowrap = require('snoowrap');
+
+    function ConfigNode(n) {
+        RED.nodes.createNode(this,n);
+        this.username = n.username;
+        this.user_agent = n.user_agent;
+        this.name = n.name;
+    }
+    RED.nodes.registerType("reddit-credentials",ConfigNode,{
+      credentials: {
+        password: {type: "password"},
+        client_id: {type: "password"},
+        client_secret: {type: "password"}
+      }
+    });
