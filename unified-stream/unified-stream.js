d4cf6b82f0718fd0118a3e2c09335e1232aea59c
sabbatiw <sabbatiw@oregonstate.edu>
2019-02-13 14:06:12 -0600

    update unified stream with node-reddit code

--- unified-stream/unified-stream.js
+++ unified-stream/unified-stream.js
@@ -1,75 +1,95 @@
-
 module.exports = function(RED) {
-    "use strict";
+  "use strict";
 
-    var mustache = require("mustache");
+  var mustache = require("mustache");
 
-    function copyPropertiesExceptMethods(newArr, originalArr) {
-        for (var i = 0; i < originalArr.length; i++){
-            var obj = JSON.stringify(originalArr[i]);
-            obj = JSON.parse(obj);
-            newArr.push({content_id: originalArr[i].id, payload: obj})
-        }
+  function copyPropertiesExceptMethods(newArr, originalArr) {
+    for (var i = 0; i < originalArr.length; i++){
+      var obj = JSON.stringify(originalArr[i]);
+      obj = JSON.parse(obj);
+      newArr.push({payload: obj})
+    }
+  }
+
+  // In order of priority: 1. HTML text OR mustache syntax, 2. msg.msgProp 
+  function parseField(msg, nodeProp, msgProp) {
+    var field = null;
+    var isTemplatedField = (nodeProp||"").indexOf("{{") != -1
+    if (isTemplatedField) {
+      field = mustache.render(nodeProp,msg);
+    }
+    else {
+      field = nodeProp || msg[msgProp];
     }
 
-    // In order of priority: 1. HTML text OR mustache syntax, 2. msg.msgProp 
-    function parseField(msg, nodeProp, msgProp) {
-        var field = null;
-        var isTemplatedField = (nodeProp||"").indexOf("{{") != -1
-        if (isTemplatedField) {
-            field = mustache.render(nodeProp,msg);
-        }
-        else {
-            field = nodeProp || msg[msgProp];
-        }
+    return field;
+  }
 
-        return field;
+  // setup the credentials for each node
+  // ex:  var node = this;
+  //      var options = parseCredentials(n);
+  const parseCredentials = (n) => {
+    let config = RED.nodes.getNode(n.reddit);
+    let credentials = config.credentials;
+    let options = {
+      userAgent: config.user_agent,
+      clientId: credentials.client_id,
+      clientSecret: credentials.client_secret
+    };
+
+    if (config.auth_type == "username_password") {
+      options.username = config.username;
+      options.password = credentials.password;
+    } else if (config.auth_type == "refresh_token") {
+      options.refreshToken = credentials.refresh_token;
+    } else if (config.auth_type == "access_token") {
+      options.accessToken = credentials.access_token;
     }
 
-    const snoowrap = require('snoowrap');
-    const snoostorm = require('snoostorm');
+    return options;
+  }
 
-    function ConfigNode(n) {
-        RED.nodes.createNode(this,n);
-        this.username = n.username;
-        this.user_agent = n.user_agent;
+  const snoowrap = require('snoowrap');
+  const snoostorm= require('snoostorm');
+
+  function ConfigNode(n) {
+    RED.nodes.createNode(this,n);
+    this.username = n.username;
+    this.user_agent = n.user_agent;
+    this.auth_type = n.auth_type;
+    this.name = n.name;
+  }
+  RED.nodes.registerType("reddit-credentials",ConfigNode,{
+    credentials: {
+      password: {type: "password"},
+      client_id: {type: "password"},
+      client_secret: {type: "password"},
+      refresh_token: {type: "password"},
+      access_token: {type: "password"}
     }
-    RED.nodes.registerType("reddit-credentials",ConfigNode,{
-      credentials: {
-        password: {type: "password"},
-        client_id: {type: "password"},
-        client_secret: {type: "password"}
-      }
-    });
+  });
 
-// subreddit stream node
+  // subreddit stream node
 
   function StreamSubreddit(n) {
     RED.nodes.createNode(this, n);
     
-    let config = RED.nodes.getNode(n.reddit);
-    let credentials = config.credentials;
     let node = this;
-    let options = {
-      userAgent: config.user_agent,
-      clientId: credentials.client_id,
-      clientSecret: credentials.client_secret,
-      username: config.username,
-      password: credentials.password
-    };
-    
+    let options = parseCredentials(n);
+   
     const r = new snoowrap(options);
     const s = new snoostorm(r);
 
     node.status({});
 
     let stream;
-
+    
     node.on('input', () => {
+      // begin displaying the stream counter
       let count = 0;
-
       node.status({fill: "blue", shape: "dot", text: n.kind + ": " + count}); 
 
+      // stream and update the stream counter
       if (n.kind === "submissions") {
         stream = s.SubmissionStream({
           subreddit: n.subreddit,
@@ -92,6 +112,7 @@ module.exports = function(RED) {
         });
       }
 
+      // stop streaming after optional user-provided timeout
       if (n.timeout != "") {
         let timeout = parseInt(n.timeout, 10);
         if ( !isNaN(timeout) ) {
@@ -103,10 +124,12 @@ module.exports = function(RED) {
       }
     });
 
+    // stop streaming if node deleted from flow
     node.on("close", () => {
       stream.emit("stop");
     });
 
+    // don't start streaming until we get user input
     if (n.kind != "" && n.subreddit != "") {
       node.emit("input", {});
     }
