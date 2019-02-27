353077ecde25f50de3577a744e17393b9bcb05f6
leqis <songle@oregonstate.edu>
2019-02-26 22:19:21 -0800

    all ready except for delete err catch

--- node-reddit.js
+++ node-reddit.js
@@ -617,9 +617,9 @@ module.exports = function(RED) {
 	
 	
 	/***** Edit Node *****/
-	function EditContent(n){
+    function EditContent(n){
         RED.nodes.createNode(this,n);
-		
+		//var config = RED.nodes.getNode(n.reddit);
 		//var credentials = config.credentials;
 		var node = this;
 		var options = parseCredentials(n);
